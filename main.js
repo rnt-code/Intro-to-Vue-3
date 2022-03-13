e1b25a61a1aed27f8bfefe29a0c5e868c7ad9c3f
Ramón Taboada <rntaboada@yahoo.com.ar>
2022-03-13 13:10:36 -0300

    Challenge completed

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks',
+            description: 'This sock sucks'
+        }
+    }
+})
+
+//a Vue.createApp le pasamos un objeto {}, con una propiedad data: cuyo valor es una función que también nos devolverá un objeto, y este tendrá una prapiedad product cuyo valor es 'Socks'
+
+//para simplificar la sintaxis también podemos hacer esto:
+
+/**
+ * const app = Vue.createApp({
+    data() {   <---------------- esto se simplifica           
+        return {
+            product: 'Socks'
+        }
+    }
+})
+ * 
+ */
