a59550c095282b3b66b89266d899ec80965f6a7d
Ramón Taboada <rntaboada@yahoo.com.ar>
2022-03-16 22:23:16 -0300

    L8-start challenge completed

--- main.js
+++ main.js
@@ -3,22 +3,40 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            brand: 'Vue Mastery', //this is the new property
+            selectedVariant: 0,
+            //image: './assets/images/socks_blue.jpg',
+            //inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            onSale: false
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        isOnSale() {
+            if(this.onSale) {
+                return this.brand + ' ' + this.product
+            }
         }
     }
 })
